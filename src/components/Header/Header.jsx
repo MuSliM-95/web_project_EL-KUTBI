764a283f303407a490ae791843d4f373203862f6
AgReSSoR <samahkinets@gmail.com>
2023-03-21 14:35:53 +0500

    Поработал с header, внес некоторые изменение, добавил кнопку каталог

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./Header.module.scss"
-import sign_in from "../../logo/sign-in.png"
+import basket from "../../logo/basket.png"
+import menu from "../../logo/Menu (2).png"
 import EL_KUTBI from "../../logo/EL-KUTBI.jpg"
-import contacts from "../../logo/contacts.png";
+import search from "../../logo/search.png";
+import catalog from "../../logo/catalog.png"
 
 
 
@@ -15,23 +17,24 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.headerBloc1}>
-                <img className={styles.El_kutbiImg} src={EL_KUTBI} alt="menu" />
+                <img className={styles.menuImg} src={menu} alt="menu" />
+                <img className={styles.El_kutbiImg} src={EL_KUTBI} alt="EL_KUTBI_logo" />
                 <strong className={styles.storeName}>EL-KUTBI</strong>
-                <img className={styles.sign_in} src={sign_in} alt="sign-in_logo" />
+                <img className={styles.basket} src={basket} alt="sign-in_logo" />
             </div>
 
             <div className={styles.headerBloc2}>
-                <form onClick={stopForm}>
-                    <input className={styles.input} type="text" />
-                    <div>
-                    <img className={styles.contacts} src={contacts} alt="contacts_logo" />
-                        <b>Обратная связь</b>
-                    </div>
-                </form>
-
+                <div>
+                    <button className={styles.catalog}><img src={catalog} alt="catalog_logo" /> <p>Каталог</p></button>
+                    <form onClick={stopForm}>
+                        <input className={styles.input} placeholder="Найти книгу или товар" type="text" />
+                        <button className={styles.search_button}><img className={styles.search} src={search} alt="search" /></button>
+                    </form>
+                </div>
             </div>
+
         </header>
     )
 }
 
-export default Header
+export default Header 
