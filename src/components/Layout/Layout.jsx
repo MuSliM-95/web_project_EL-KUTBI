da33ed24419aff53b5951709e3baff9a44403249
AgReSSoR <samahkinets@gmail.com>
2023-03-22 20:45:52 +0500

    Убрал ненужные импорты

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import styles from "./Layout.module.scss"
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-// import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import Main from "../Content/Main/Main";
+
 
 
 const Layout = () => {
