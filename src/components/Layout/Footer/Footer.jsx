9caef96d5191fb28aa7744ce00a50361dad992aa
AgReSSoR <samahkinets@gmail.com>
2023-03-22 21:14:01 +0500

    Настроил ссылки мессенджеров для обратной связи

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
                 <h3>Книжный интернет магазин</h3>
                 <h2>‹‹EL-KUTBI››</h2>
                 <div className={styles.messengers}>
-                    <a href="https://wa.me/79604416212" target={"_blank"}><img src={whatsapp} alt="whatsapp_logo" /></a>
-                    <a href="https://t.me/EL_KUTB_Bot" target={"_blank"}><img src={telegram} alt="telegram_logo" /></a>
-                    <a href="https://instagram.com/el_kutbi?igshid=YmMyMTA2M2Y=" target={"_blank"}><img src={instagram} alt="instagram_logo" /></a>
+                    <a href="https://wa.me/79604416212" target={"_blank"} rel={"noreferrer"}><img src={whatsapp} alt="whatsapp_logo" /></a>
+                    <a href="https://t.me/EL_KUTB_Bot" target={"_blank"} rel={"noreferrer"}><img src={telegram} alt="telegram_logo" /></a>
+                    <a href="https://instagram.com/el_kutbi?igshid=YmMyMTA2M2Y=" target={"_blank"} rel={"noreferrer"}><img src={instagram} alt="instagram_logo" /></a>
                 </div>
             </div>
             <div className={styles.aboutus}>
