3396c3152a1ecc5056a715734e7caebc0df2e041
AgReSSoR <samahkinets@gmail.com>
2023-03-26 20:39:38 +0500

    Удаляю ненужные файлы, которые мешают запустить на тестовый сервер

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Main from "../../components/Content/Main/Main";
-import Navigation from "../../components/Layout/Sidebar/Navigation";
 import styles from "./MainPage.module.scss"
 
 const MainPage = () => {
     return (
         <div className={styles.MainPageContainer}>
-            <Navigation/>
             <Main/>
         </div>
     )
