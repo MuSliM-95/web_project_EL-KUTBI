29f9fb19bfdd3d7f6b02e1892fef330584b7ab78
AgReSSoR <samahkinets@gmail.com>
2023-03-26 21:38:01 +0500

    Обновление

--- src/components/Content/ProductList/ProductList.jsx
+++ src/components/Content/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../../app/AsyncFetch/productsFetch";
 import ProductButton from "./ProductButton/ProductButton";
