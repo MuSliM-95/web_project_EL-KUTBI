da7f70daac8e065b1360dc5ff6fab61f2ca420ae
AgReSSoR <samahkinets@gmail.com>
2023-05-05 17:48:08 +0300

    Решил проблему с  погрузок иконок меню при переходе.

--- src/components/Layout/FixedMenu/FixedMenu.jsx
+++ src/components/Layout/FixedMenu/FixedMenu.jsx
@@ -1,18 +1,20 @@
-import React, { useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { Link, useMatch } from "react-router-dom";
 import Menu from "../../Content/Menu/Menu";
 import styles from "./FixedMenu.module.scss";
 
 const FixedMenu = () => {
-  const menu = useRef();
+  const menu = useRef(null);
+  const [menuButton, setMenuButton] = useState(false);
 
   const menuUpdate = () => {
+    setMenuButton(!menuButton);
     menu.current.classList.toggle(styles.menuActive);
     return menu.current.classList.toggle(styles.menuNone);
   };
 
   const menuСlose = (e) => {
-    console.log(e.target.classList.toggle("active"));
+    setMenuButton(!menuButton);
     menu.current.classList.add(styles.menuNone);
     return menu.current.classList.remove(styles.menuActive);
   };
@@ -25,27 +27,47 @@ const FixedMenu = () => {
       <Link
         onClick={menuСlose}
         to={"/"}
-        className={styles.FixedMenuLinkHome}
+        className={
+          useMatch("/") && !menuButton
+            ? styles.FixedMenuLinkHomeActive
+            : styles.FixedMenuLinkHome
+        }
       ></Link>
       <button
         type="button"
-        className={styles.FixedMenuButtonMenu}
+        className={
+          menuButton
+            ? styles.FixedMenuButtonMenuActive
+            : styles.FixedMenuButtonMenu
+        }
         onClick={menuUpdate}
       ></button>
       <Link
         onClick={menuСlose}
-        className={styles.FixedMenuLinkBasket}
+        className={
+          useMatch("/basket") && !menuButton
+            ? styles.FixedMenuLinkBasketActive
+            : styles.FixedMenuLinkBasket
+        }
         to={"/basket"}
       ></Link>
       <Link
         onClick={menuСlose}
-        className={styles.FixedMenuLinkSelected}
+        className={
+          useMatch("/favorites") && !menuButton
+            ? styles.FixedMenuLinkSelectedActive
+            : styles.FixedMenuLinkSelected
+        }
         to={"/favorites"}
       ></Link>
       <Link
         onClick={menuСlose}
         to={"/usersAccount"}
-        className={styles.FixedMenuLinkPersonalAccount}
+        className={
+          useMatch("/usersAccount") && !menuButton
+            ? styles.FixedMenuLinkPersonalAccountActive
+            : styles.FixedMenuLinkPersonalAccount
+        }
       ></Link>
     </div>
   );
