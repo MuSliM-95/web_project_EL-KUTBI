6b5cb52fc51c7f30154d79fdc8929b105cf6673c
AgReSSoR <samahkinets@gmail.com>
2023-05-25 02:56:22 +0300

    Обновление

--- src/serverUrl/serverUrl.js
+++ src/serverUrl/serverUrl.js
@@ -2,8 +2,9 @@ const { NODE_ENV } = process.env;
 
 export let serverUrl;
 
-// if (NODE_ENV === "development") {
-//   serverUrl = "https://el-kutbi-backend.onrender.com";
-// } else {
-  serverUrl = "http://localhost:6001";
-// }
+if (NODE_ENV === "development") {
+  serverUrl = "http://45.12.74.138:6001";
+  // serverUrl = "https://el-kutbi-backend.onrender.com";
+} else {
+  serverUrl = "http://45.12.74.138:6001";
+}
