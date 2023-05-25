03c68e60652406b5933a9d734a00e3e3361e643c
AgReSSoR <samahkinets@gmail.com>
2023-05-25 03:52:43 +0300

    Обновление

--- src/components/Content/FavoritesButton/FavoritesButton.jsx
+++ src/components/Content/FavoritesButton/FavoritesButton.jsx
@@ -3,19 +3,23 @@ import styles from "./FavoritesButton.module.scss";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../../../app/AsyncFetch/favoritesFetch";
+import { useNavigate } from "react-router-dom";
 
 const FavoritesButton = ({ el }) => {
   const dispatch = useDispatch();
+const navigate = useNavigate()
+
   const favoritesProduct = useSelector(
     (state) => state.favoritesReducer.productFavorites
   );
   const userId = useSelector((state) => state.usersReducer.userId);
+  const token = useSelector((state) => state.usersReducer.token);
+
   const filter = favoritesProduct?.some((item) => item._id === el._id);
 
   const addProductFavorites = () => {
-    if (!filter) {
-      return dispatch(addItem({ userId, item: el }));
-    }
+    if(!token) navigate("signinUp")
+    if (!filter) dispatch(addItem({ userId, item: el }));
   };
 
   return (
--- src/serverUrl/serverUrl.js
+++ src/serverUrl/serverUrl.js
@@ -2,9 +2,9 @@ const { NODE_ENV } = process.env;
 
 export let serverUrl;
 
-// if (NODE_ENV === "development") {
+if (NODE_ENV === "development") {
   serverUrl = "http://45.12.74.138:6001";
   // serverUrl = "https://el-kutbi-backend.onrender.com";
-// } else {
-  // serverUrl = "http://45.12.74.138:6001";
-// }
+} else {
+  serverUrl = "http://localhost:6001";
+}
