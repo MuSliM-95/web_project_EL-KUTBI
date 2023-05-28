fd81774903fea37ddcefaeae2f27273b5c6ec5ba
AgReSSoR <samahkinets@gmail.com>
2023-05-28 17:35:31 +0300

    Обновление

--- src/serverUrl/serverUrl.js
+++ src/serverUrl/serverUrl.js
@@ -1,10 +1,9 @@
-// const { NODE_ENV } = process.env;
+const { NODE_ENV } = process.env;
 
 export let serverUrl;
 
 if (NODE_ENV === "development") {
-  serverUrl = "http://localhost:6001";
-} else {
   serverUrl = "https://el-kutbi-backend.onrender.com";
-
+} else {
+  serverUrl = "http://localhost:6001";
 }
